efecae4ef66289db1272054a6d2e71eb3503fbac
osauli-0 <osauli-0@student.ltu.se>
2021-11-15 16:04:30 +0100

    removed rec

--- functions.py
+++ functions.py
@@ -167,31 +167,7 @@ def merge(L1: list, L2: list):
     merged += L2
     return merged
 
-def binary_search(arr, low, high, x):
- 
-    # Check base case
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        # If element is present at the middle itself
-        if arr[mid] == x:
-            return mid
- 
-        # If element is smaller than mid, then it can only
-        # be present in left subarray
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
- 
-        # Else the element can only be present in right subarray
-        else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        # Element is not present in the array
-        return -1
-
-def binary_search2(lst, length, key):
+def binary_search(lst, length, key):
     low = 0
     high = length
     while(low <= high):
@@ -211,7 +187,7 @@ def bSort(lst):
         return lst
     for i in range(1, len(lst)):
         key = lst[i]
-        position = binary_search(lst,0, i, key)
+        position = binary_search(lst, i, key)
         lst.insert(position, key)
         del lst[i]
     return lst
