26d4823e4a6eeb2c4937152cdcbe6b91837f94e2
Ivar Jönsson <49920136+ivario123@users.noreply.github.com>
2021-11-15 15:46:19 +0100

    bruh

--- lab1.py
+++ lab1.py
@@ -26,7 +26,7 @@ def test_k():
     vals = list(numpy.random.randint(0, n, n))
     vals_sorted = sorted(vals)
     vals_almost_sorted = vals_sorted[:len(vals_sorted)//2]+vals[len(vals)//2::-1]
-    for i in range(1,1000,10):
+    for i in range(1,1000,5):
         if is_sorted(vals):
             vals = list(numpy.random.randint(0, n, n))
         print(f"k test : Running tests for k = {i}",end = '\r')
