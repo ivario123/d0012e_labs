7c0182cfeab3578b93f92b48ec4d4486f878f49d
Ivar Jönsson <49920136+ivario123@users.noreply.github.com>
2021-11-22 16:07:45 +0100

    bruh

--- lab2/Rust_impl/main.rs
+++ lab2/Rust_impl/main.rs
@@ -1,5 +1,4 @@
 fn smallest_three_incremental(L: Vec<i16>) -> Vec<i16> {
-  /// Finds the smalles three elements in an array using an incremental aproach
   if L.len() < 3{
     return L;
   }
@@ -18,10 +17,42 @@ fn smallest_three_incremental(L: Vec<i16>) -> Vec<i16> {
   return ret;
 }
 
-fn smallest_three_divide(L:Vec<i16>) -> Veck<i16>{
-  if L.len() ==1
+fn smallest_three_divide(L:Vec<i16>) -> Vec<i16>{
+  let len = L.len() as i16;
+  if L.len() <=1{
     return L;
-  
+  }
+  let mut mid : i16 = len/2 as i16;
+  let mut left : Vec<i16> = smallest_three_divide(L[0..mid]);
+  let mut right : Vec<i16> = smallest_three_divide(L[mid..L.len]);
+  let max_value : i16 =  if len > 3 {3} else { len};
+  let mut ret = Vec![];
+  for i in 0..max_value{
+    if left.len() == 0{
+      while ret.len() < max_value{
+        ret.append(right[0]);
+        right.erase(right.begin());
+      }
+      return ret;
+    }
+    else if right.len() == 0{
+      while ret.len() < max_value{
+        ret.append(left[0]);
+        left.erase(left.begin());
+      }
+      return ret;
+
+    }
+    if left[0]<right[0]{
+       ret.append(left[0]);
+       left.erase(left.begin());
+    }
+    else{
+      ret.append(right[0]);
+      right.erase(right.begin());
+    }
+  }  
+  return ret;
 }
 
 
@@ -30,4 +61,5 @@ fn smallest_three_divide(L:Vec<i16>) -> Veck<i16>{
 fn main(){
   let vec :Vec<i16> = [1,2,3,5,6,-2,-100,-5,-6].to_vec();
   println!("{:?}",smallest_three_incremental(vec));
+  println!("{:?}",smallest_three_divide(vec));
 }
--- lab2/functions.py
+++ lab2/functions.py
@@ -16,20 +16,21 @@ def smallest_three_incremental(L: list) -> list:
       ### param L : list of elements
       ### return : list of three smallest elements
     """
-    if len(L) < 3:
-        return L
-    ret = [float('inf'), float('inf'), float('inf')]
-    for el in L:
-      if el < ret[0]:
-        ret.insert(0, el)
-        ret.pop()
-      elif el < ret[1]:
-        ret.insert(1, el)
-        ret.pop()
-      elif el < ret[2]:
-        ret.insert(2, el)
-        ret.pop()
+    if len(L) < 3:                                                  # C1
+        return L                                                    # C2
+    ret = [float('inf'), float('inf'), float('inf')]                # C3
+    for el in L:                                                    # T(n)
+        if el < ret[0]:                                             # C4
+            ret[2] = ret[1]                                         # C5
+            ret[1] = ret[0]                                         # C6
+            ret[0] = el                                             # C7
+        elif el < ret[1]:                                           # C8
+            ret[2] = ret[1]                                         # C9
+            ret[1] = el                                             # C10
+        elif el < ret[2]:                                           # C11
+            ret[2] = el                                             # C12
     return ret
+
 
 def smallest_three_divide_and_conquer(L: list) -> list:
     """
@@ -44,24 +45,20 @@ def smallest_three_divide_and_conquer(L: list) -> list:
     left = smallest_three_divide_and_conquer(L[:mid])
     right = smallest_three_divide_and_conquer(L[mid:])
     ret = []
-    right_index = 0
-    left_index = 0
     max_value = length if length < 3 else 3
-    while length < max_value and left and right:
-      if left[0] < right[0]:
-        ret.append(left[0])
-        del left[0]
-      else:
-        ret.append(right[0])
-        del right[0]
-    if len(ret) != max_value:
-      ret.extend(left)
-      ret.extend(right)
+    while len(ret) < max_value and left and right:
+        if left[0] < right[0]:
+            ret.append(left[0])
+            del left[0]
+        else:
+            ret.append(right[0])
+            del right[0]
+    if len(ret) < max_value:
+        ret.extend(left)
+        ret.extend(right)
 
     return ret
 
-
-    
 
 """
 Given an array A=a1,a2,···,an of non-zero real numbers, the problem is to find a
@@ -73,25 +70,54 @@ implement your algorithm. Built-in functions or methods for strings or lists mus
 used. Your algorithm should run in O(n)time in the worst case. You may assume that
 n=2k for some positive integer k.
 """
-def max_subarray(L: list) -> int:
+
+
+def max_subarray(L: list) -> tuple:
     """
       Finds the maximum sum of a subarray in the list L
       ### param L: list of elements
-      ### return : sum of largest sublist
+      ### return : sum of largest sublist and wether it chose left, right or middle
     """
     if len(L) == 1:
-      return L[0]
-    if len(L) == 2: # base case since a list of one elements max subsum is the element itself
-        return L[0] if L[0] > L[1] else L[1]
+        return (L[0],0)
     mid = len(L) // 2
-    left = max_subarray(L[:mid])
-    right = max_subarray(L[mid:])
-    
-    # Checking if left sum is greater than right sum or if sum of left and right is greater than left and right
-    #if left + right> left and left + right > right:
-    #    return left + right
+    left, l_choise = max_subarray(L[:mid])
+    right, r_choise = max_subarray(L[mid:])
+
+    if (r_choise == 0 or r_choise == -1) and (l_choise == 1 or l_choise == 0):
+        # Checking if left sum is greater than right sum or if sum of left and right is greater than left and right
+        if left + right > left and left + right > right:
+          return (left + right, 0)
     if right > left:
-        return right
+        return (right, 1)
     else:
-        return left
-        
+        return (left, -1)
+
+
+if __name__ == "__main__":
+    def assert_three_smallest_incremental(
+        L, ans): return smallest_three_incremental(L) == ans
+    def assert_smallest_three_divide_and_conquer(
+        L, ans): return smallest_three_divide_and_conquer(L) == ans
+
+    def assert_max_subarray(L, ans): return max_subarray(L)[0] == ans
+    print(
+        f'Incremental approch to three smallest gave the ouput : {smallest_three_incremental([11,-2,1,2,3,4,5,6,7,8,9,10])}')
+    print(
+        f'Incremental approch to three smallest works : {assert_three_smallest_incremental([11,-2,1,2,3,4,5,6,7,8,9,10], [-2,1,2])}')
+    """
+    Testing the min array
+  """
+    print(
+        f'Divide and conquer approch to three smallest gave the output : {smallest_three_divide_and_conquer([11,-2,1,2,3,4,5,6,7,8,9,10])}')
+    print(
+        f'Divide and conquer approch to three smallest works : {assert_smallest_three_divide_and_conquer([11,-2,1,2,3,4,5,6,7,8,9,10], [-2,1,2])}')
+
+    """
+    Testing the max array
+  """
+
+    print(
+        f'Max subarray gives the output : {max_subarray([-2,1,2,3,4,5,-10,6,7,8,9,10])}')
+    print(
+        f'Max subarray works : {assert_max_subarray([-2,1,2,3,4,5,-10,6,7,8,9,10], sum([6,7,8,9,10]))}')
