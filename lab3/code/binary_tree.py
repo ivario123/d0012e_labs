78f1aab988e19cd623f4bb3c238b15db3b4747c9
Ivar Jönsson <49920136+ivario123@users.noreply.github.com>
2021-12-13 15:32:39 +0100

    bruh

--- lab3/code/binary_tree.py
+++ lab3/code/binary_tree.py
@@ -1,53 +1,100 @@
-import numpy as np
+
+
+
 class binary_tree_node:
-    def __init__(self,value:int=None,number_of_children:list = None,parent = None,left = None,right = None) -> None:
+    def __init__(self, value: int = None, number_of_children: list = None, parent=None, left=None, right=None) -> None:
         self.value = value
-        if number_of_children:
-          self.number_of_children = number_of_children
-        else:
-          self.number_of_children = [0,0,0]
+        self.number_of_children = [0, 0, 0]
         self.parent = parent
         self.right = right
         self.left = left
-
-    def insert_child(self,value):
-      if value > self.value:
-        self.right =  binary_tree_node(value,parent=self)
-        self.number_of_children[1] +=1
-      else:
-        self.left = binary_tree_node(value,parent=self)
-        self.number_of_children[0] +=1
-      self.number_of_children[2]+=1
-      return
+    """
+      Update a nodes child counter, should be called for entier anscestry after a insertion
+    """
 
     def update_child_counter(self):
-      if self.right:
-        self.number_of_children[1] = self.right.number_of_children[2]+1
-      else:
-        self.number_of_children[1] = 0
-      if self.left:
-        self.number_of_children[0] = self.left.number_of_children[2]+1
-      else:
-        self.number_of_children[0] = 0
-      self.number_of_children[2] = self.number_of_children[1]+self.number_of_children[0]
-      return
-    def unbalanced(self,c):
-      if self.number_of_children[0] > c*self.number_of_children[2]:
-        return 1
-      if self.number_of_children[1] > c*self.number_of_children[2]:
-        return 1
-      return 0
-    def in_order_walk(self):
-      if self.left:
-        ret = self.left.in_order_walk()
-      else:
-        ret = []
-      ret.append(self.value)
-      if self.right:
-        ret.extend(self.right.in_order_walk())
-      return ret
-      
+        if self.right:
+            self.number_of_children[1] = self.right.number_of_children[2]+1
+        else:
+            self.number_of_children[1] = 0
+        if self.left:
+            self.number_of_children[0] = self.left.number_of_children[2]+1
+        else:
+            self.number_of_children[0] = 0
+        self.number_of_children[2] = self.number_of_children[1] + \
+            self.number_of_children[0]
+        return
+    """
+      Checks if a tree is unbalanced, returns 1 if unbalanced 0 if not
+    """
 
+    def unbalanced(self, c):
+        if abs(self.number_of_children[1] - self.number_of_children[0]) <= 1:
+            """
+              This captures the unbalanceable case where left side has 1 element and right side has 0
+            """
+            return 0
+        if self.number_of_children[0] > c*self.number_of_children[2]:
+            return -1
+        if self.number_of_children[1] > c*self.number_of_children[2]:
+            return 1
+        return 0
+
+    def in_order_walk(self):
+        if self.left:
+            ret = self.left.in_order_walk()
+        else:
+            ret = []
+        ret.append(self.value)
+        if self.right:
+            ret.extend(self.right.in_order_walk())
+        return ret
+
+    """
+      Only used to validate that large trees are balanced too
+    """
+
+    def is_valid(self, c):
+        ret = self.unbalanced(c) == 0
+        if self.left:
+            ret = ret == self.left.is_valid(c) == True
+        if self.right:
+            ret = ret == self.right.is_valid(c) == True
+        return ret
+    
+    def search(self,value):
+      if self.value == value:
+        return self
+      if value > self.value:
+        if self.right:
+          return self.right.search(value)
+        return None
+      if self.left:
+        return self.left.search(value)
+      return None
+    
+    def find_extreme(self):
+        if self == None:
+          print("Bruh")
+        if self.left == self.right == None:
+            return self, 0
+        left_best, left_depth, right_best, right_depth = None, 0, None, 0
+        if self.left!=None:
+            left_best, left_depth = self.left.find_extreme()
+        if self.right!=None:
+            right_best, right_depth = self.right.find_extreme()
+        if not left_best or not right_best:
+          if left_best:
+            return left_best,left_depth+1
+          else:
+            return right_best,right_depth+1
+        if left_depth > right_depth:
+            best = left_best
+            best_depth = left_depth+1
+        else:
+            best = right_best
+            best_depth = right_depth+1
+        return best, best_depth
 
     """
       Some display code joinked from stack overflow, modernized a bit but still the same
@@ -109,71 +156,113 @@ class binary_tree_node:
         lines = [first_line, second_line] + \
             [a + u * ' ' + b for a, b in zipped_lines]
         return lines, n + m + u, max(p, q) + 2, n + u // 2
-      
+
+
 class binary_tree:
-  def __init__(self,c,root:binary_tree_node=None) -> None:
-      self.root = root
-      self.c = c
-  def insert(self,value):
-      if not self.root:
-        self.root = binary_tree_node(value)
-        return
-      current_node = self.root
-      while 1:
-        if value > current_node.value:
-          if not current_node.right:
-            break
-          current_node = current_node.right
-        else:
-          if not current_node.left:
-            break
-          current_node = current_node.left
-      node = binary_tree_node(value,parent=current_node)
-      if value > current_node.value:
-        current_node.right = node
-      else:
-        current_node.left = node
-      node.update_child_counter()
-      while current_node:
-        current_node.update_child_counter()
-        child = current_node
-        current_node = current_node.parent
-        
-        if child.unbalanced(self.c):
-          child = binary_tree.balanced(self.c,child)
-          if current_node:
-            if child.value > current_node.value:
-              current_node.right = child
-            else:
-              current_node.left = child
-            
-          else:
-            self.root = child
+    def __init__(self, c, root: binary_tree_node = None) -> None:
+        self.root = root
+        self.c = c
+        self.displaying = False
+        self.uses_balance = True
+
+    def insert(self, value):
+        # If the tree is empty insert a new node at the root
+        if not self.root:
+            self.root = binary_tree_node(value)
             return
-      
-  def in_order_walk(self):
-    return self.root.in_order_walk()
-  def balanced(c,node,return_type = "node"):
+        # Otherwise start looking at
+        current_node = self.root
+        # Find where to insert the new node
+        while 1:
+            if value > current_node.value:
+                if not current_node.right:
+                    break
+                current_node = current_node.right
+            else:
+                if not current_node.left:
+                    break
+                current_node = current_node.left
+        # Insert the new node
+        node = binary_tree_node(value, parent=current_node)
+        if value > current_node.value:
+            current_node.right = node
+        else:
+            current_node.left = node
+
+        if self.uses_balance:
+            node.update_child_counter()
+            while current_node:
+                current_node.update_child_counter()
+                current_node = current_node.parent
+
+            node = self.root
+            # Costs worst case O(nlogn)
+            self.itterative_rebalance_check(node, value)
+
+    def insert_list(self, L: list):
+        for el in L:
+            self.insert(el)
+
+    def itterative_rebalance_check(self, node, value):
+        while node:
+            if node.unbalanced(self.c):
+                if self.displaying:
+                    print("*"*100)
+                    node.display()
+                # Balance that node
+                # O (n) since sorting is O(n) and the insertion is O(n) and each one is done one time, O(2n) = O(n)
+                new_node = binary_tree.rebalance_tree(self.c, node)
+
+                # Overwrite anscetry
+                if not node.parent:
+                    self.root = new_node
+                else:
+                    if new_node.value > node.parent.value:
+                        node.parent.right = new_node
+                    else:
+                        node.parent.left = new_node
+                node = new_node
+                if self.displaying:
+                    node.display()
+                    print("#"*100)
+                return
+
+            # Move down the tree if no unsorted node is found, maximum number of times is h
+            if value > node.value:
+                node = node.right
+            else:
+                node = node.left
+
+    def search(self, value):
+        return self.root.search(value)
+
+    def is_valid(self):
+        return self.root.is_valid(self.c)
+
+    def in_order_walk(self):
+        return self.root.in_order_walk()
+
+    def rebalance_tree(c, node, return_type="node"):
         """
             Balances a BST using insertion of a balanced binary search tree
         """
         arr = node.in_order_walk()
         new_root = binary_tree.sorted_array_to_bst(arr)
-        tree = binary_tree(c,new_root)
+        tree = binary_tree(c, new_root)
         tree.root.parent = node.parent
         if return_type == "node":
-          return tree.root
+            return tree.root
         return tree
 
-  def sorted_array_to_bst(arr):
+    def sorted_array_to_bst(arr):
 
         if not arr:
             return None
         mid = (len(arr)) // 2
         root = binary_tree_node(arr[mid])
-        root.left= binary_tree.sorted_array_to_bst(
+        root.left = binary_tree.sorted_array_to_bst(
             arr[:mid])
-        root.right= binary_tree.sorted_array_to_bst(
+        root.right = binary_tree.sorted_array_to_bst(
             arr[mid+1:])
         root.update_child_counter()
         if root.right != None:
@@ -181,15 +270,10 @@ class binary_tree:
         if root.left != None:
             root.left.parent = root
         return root
-  
-  def display(self):
-    if self.root:
-      self.root.display()
+
+    def display(self):
+        if self.root:
+            self.root.display()
 
 
-if __name__ == "__main__":
-  tree = binary_tree(0.5)
-  data = list(np.random.randint(-100,100,50))
-  for el in data:
-    tree.insert(el)
-  tree.display()
+
---
+++ lab3/code/tests.py
@@ -0,0 +1,182 @@
+from  binary_tree import *
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import os
+from optparse import OptionParser
+import inspect
+class tests:
+    def __init__(self, input_data: list = list(np.random.randint(-100, 100, 60)), c_range: list = [0.51, 1.0], c_interval: int = 0.01, length_range=[10**3, 5*10**4], length_interval=2*10**3):
+        self.input_data = input_data
+        self.c_range = c_range
+        self.c_interval = c_interval
+        self.results = {"validate_functions": [], "constant_c_varying_length": [], "lengths_tested": [
+        ], "constant_length_varying_c": [], "c_values_tested": [], "normal_bst": [], "length_tested_bst": [],
+        "unbalanced_search_time":[],"balanced_search_time":[]}
+        self.length_range = length_range
+        self.length_interval = length_interval
+        return
+    def show_for_average_c(self):
+      print("...___...___"*(100//12))
+      c = self.c_range[0]+self.c_range[1]
+      c/=2
+      tree = binary_tree(c)
+      tree.displaying = True
+      tree.insert_list(self.input_data)
+      tree.display()
+    def plot(self, x: list, y: list, x_label: str, y_label: str, plot_header: str):
+        fig = plt.figure()
+        plt.title(plot_header, fontsize='16')
+        if len(y[0])> 1:
+          plt.plot(x,y[0])
+          plt.plot(x,y[1])
+        else:
+          plt.plot(x, y)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.grid()
+        # Figures out the absolute path for you in case your working directory moves around.
+        my_path = os.path.dirname(os.path.abspath(__file__))
+        fig.savefig(my_path + f"/plots/{plot_header.replace(' ','_')}.png")
+
+    def test_normal_bst(self):
+        print("*"*100)
+        print("Testing a normal bst with varying input length")
+        for length in range(self.length_range[0], self.length_range[1], self.length_interval):
+            print(f"Testing for a normal BST with input length {length}")
+            tree = binary_tree()
+            tree.uses_balance = False
+            data = list(np.random.randint(-length, length, length))
+            time_1 = time.time()
+            tree.insert_list(data)
+            time_2 = time.time()
+            self.results["normal_bst"].append(time_2-time_1)
+            self.results["length_tested_bst"].append(length)
+
+        x = list(self.results["length_tested_bst"])
+        y = list(self.results["normal_bst"])
+        self.plot(x, y,
+                  "Input_length", "Execution time [s]", f"Execution time as a function of input length for a standard bst ")
+    """
+    def test_compare_search(self):
+        print(
+            f"Comparing search time between normal bst and balanced bst\nUsing length = {self.length_range[1]//2}")
+        data = list(
+            np.random.randint(-self.length_range[1], self.length_range[1], self.length_range[1]//2))
+        tree_not_balanced = binary_tree(0)
+        tree_not_balanced.uses_balance = False
+        tree_not_balanced.insert_list(data)
+        if self.results["c_values_tested"] == []:
+          log_c = True
+        else:
+          log_c = False
+        num_to_find = tree_not_balanced.root.find_extreme()[0]
+        print(f"Number to find {num_to_find.value}")
+        for c in list(np.arange(self.c_range[0], self.c_range[1], self.c_interval)):
+            print(f"Running test for c = {c}")
+            tree_balanced = binary_tree(c)
+            tree_balanced.insert_list(data)
+            time_1_balanced = time.time()
+            tree_balanced.search(num_to_find.value)
+            time_2_balanced = time.time()
+            tree_not_balanced.search(num_to_find.value)
+            time_unbalanced = time.time()
+            self.results["balanced_search_time"].append(time_2_balanced-time_1_balanced)
+            self.results["unbalanced_search_time"].append(time_unbalanced-time_2_balanced)
+            if log_c:
+              self.results["c_values_tested"].append(c)
+            
+        
+        x = list(self.results["c_values_tested"])
+        y = [self.results["balanced_search_time"],self.results["unbalanced_search_time"]]
+        self.plot(x, y,
+                  "c value", "Execution time [s]", f"Search time for a balanced bst as a function of unbalance ")
+    """
+
+    def test_validate_functions(self):
+        """
+          Verifies that the functions work for a c in the range
+        """
+        print("*"*100)
+        print("Validating that the functions still produce a valid result")
+        tree = binary_tree(self.c_range[0])
+        tree.displaying = True
+        tree.insert_list(self.input_data)
+        self.results["validate_functions"].append(tree.is_valid())
+        return self.results["validate_functions"]
+
+    def test_constant_c_varying_length(self):
+        print("*"*100)
+        c = self.c_range[1]+self.c_range[0]
+        c /= 2
+        print("Running the tests for constant_c_varying_length")
+        for length in range(self.length_range[0], self.length_range[1], self.length_interval):
+            print(f"Testing with c = {c} for length = {length} ")
+            self.input_data = list(np.random.randint(-length, length, length))
+            tree = binary_tree(c)
+            time_1 = time.time()
+            tree.insert_list(self.input_data)
+            time_2 = time.time()
+            self.results["constant_c_varying_length"].append(time_2-time_1)
+            self.results["lengths_tested"].append(length)
+            print(f"Run took {time_2-time_1} s")
+            print("- - "*25)
+        x = list(self.results["lengths_tested"])
+        y = list(self.results["constant_c_varying_length"])
+        self.plot(x, y,
+                  "Input_length", "Execution time [s]", f"Execution time as a function of input length for c = {(self.c_range[1]+self.c_range[0])/2} ")
+
+    def test_constant_length_varying_c(self):
+        print("*"*100)
+        print("Running tests for constant_length_varying_c")
+        length = 10**4
+        
+        if self.results["c_values_tested"] == []:
+          log_c = True
+        else:
+          log_c = False
+        self.input_data = list(np.random.randint(-length, length, length))
+        for c in list(np.arange(self.c_range[0], self.c_range[1], self.c_interval)):
+            print(f"Testing with c = {c} for length = {length}")
+            tree = binary_tree(c)
+            time_1 = time.time()
+            tree.insert_list(self.input_data)
+            time_2 = time.time()
+            self.results["constant_length_varying_c"].append(time_2-time_1)
+            if log_c:
+              self.results["c_values_tested"].append(c)
+            print(f"Run took {time_2-time_1} s")
+            print("- - "*25)
+        x = list(self.results["c_values_tested"])
+        y = list(self.results["constant_length_varying_c"])
+        self.plot(x, y,
+                  "c value", "Execution time [s]", f"Execution time as a function of c for input length =  {length} ")
+
+    def run_all_tests(self):
+        functions = inspect.getmembers(self, predicate=inspect.ismethod)
+        for key, value in functions:
+            if 'test' in key and key != "run_all_tests":
+                value()
+        test.dump_to_csv("latest_run")
+
+    def dump_to_csv(self, csv_name):
+        df = [pd.DataFrame({k: v}) for k, v in self.results.items()]
+
+        df = pd.concat(df, axis=0)
+        print(df)
+        #df = pd.DataFrame.from_dict(self.results)
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_folder = 'csv_files'
+        file_path = os.path.join(file_dir, csv_folder, f'{csv_name}.csv')
+        df.to_csv(file_path, header=False, index=False)
+        pass
+
+
+if __name__ == "__main__":
+    test = tests()
+    test.test_validate_functions()
+    assert test.results["validate_functions"][0]
+    #test.run_all_tests()
+    #print(test.results)
+    test.show_for_average_c()
